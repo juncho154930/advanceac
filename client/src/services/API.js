1a2f3aa12212f3c393101a8c5d0f69bfdf9d2467
jcho <jcho@propulsionlabs.com>
2019-05-27 19:30:56 -0500

    test

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,26 +1,9 @@
-/* eslint-disable */
 import Axios from 'axios';
- 
+
 const RESOURCE_NAME = '/todo';
- 
+
 export default {
   getAll() {
     return Axios.get(RESOURCE_NAME);
   },
- 
-  get(id) {
-    return Axios.get(`${RESOURCE_NAME}/${id}`);
-  },
- 
-  create(data) {
-    return Axios.post(RESOURCE_NAME, data);
-  },
- 
-  update(id, data) {
-    return Axios.put(`${RESOURCE_NAME}/${id}`, data);
-  },
- 
-  delete(id) {
-    return Axios.delete(`${RESOURCE_NAME}/${id}`);
-  }
 };
